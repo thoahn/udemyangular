980b86858995ca900cf574d49f0e3e0f99568e32
thoahn <dogandagbasti@gmail.com>
2018-06-23 22:14:35 +0300

    dfsgfdgd

--- UdemyAngular/Content/js/directives/helloWorld/helloWorld.js
+++ UdemyAngular/Content/js/directives/helloWorld/helloWorld.js
@@ -1,7 +1,6 @@
 ﻿/// <reference path="C:\DOGAN\PROJELER\UdemyAngular\UdemyAngular\scripts/angular.js" />
 /// <reference path="../../modules/mainApp.js" />
-mainApp.directive('helloWorld', () => {
-    
+mainApp.directive('helloWorld', () => {    
     return {
         template : 'Hello world'
     };
